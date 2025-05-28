15a61368fa535cdb4586f0d26ffc10b11bdfbae5
loarsaw <amanahmed1385@gmail.com>
2025-05-28 11:36:33 +0530

    fix:removed obs

--- backend/app/routers/wiki.py
+++ backend/app/routers/wiki.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter, Query
-from app.controllers.wiki_controllers import scrape_wikipedia_title  
-
 router = APIRouter(prefix="/scrape", tags=["scrape"])
 
 @router.get("/")
